4637b07ac9372f36d0f0c25e871e47ef80369e7a
Gakyeong <qeiqiem@gmail.com>
2021-07-15 17:02:30 +0900

    greeting h1, classList add remove

--- app.js
+++ app.js
@@ -7,8 +7,14 @@
 
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-function onLoginSumit(event) {
+const HIDDEN_CLASSNAME = 'hidden';
+// * uppercase
+// string만 포함한 변수를 담을 때는 대문자로 변수이름을 짓는다. (관습)
+// login~, greeting처럼 중요한 정보를 담은 게 아니기 때문에
+
+function onLoginSubmit(event) {
     // default 동작 : submit -> refresh 방지!
     event.preventDefault();
 
@@ -19,6 +25,12 @@ function onLoginSumit(event) {
     // 해당 event를 preventDegfault()하는 작동을 연결해줌
     // 그래서 그 서브밋을 방지하는 작동을 하게 된다.
     // console.log(event)로 어떤 이벤트가 실행됐는지 알 수 있음
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+
+    greeting.innerText = `Hello, ${userName}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
