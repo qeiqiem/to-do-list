60d2fcbf729de74cce6987bea4dde47d1414b486
Gakyeong <qeiqiem@gmail.com>
2021-07-19 20:49:01 +0900

    quotes added

--- js/quotes.js
+++ js/quotes.js
@@ -1,42 +1,42 @@
 const quotes = [
     {
-        quotes: '',
-        author: '',
+        quotes: "Work out your own salvation. Do not depend on others.",
+        author: "Buddha",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "Do not look for a sanctuary in anyone except your self.",
+        author: "Buddha",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.",
+        author: "Buddha",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "All that we are is the result of what we have thought.",
+        author: "Buddha",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "If you tell the truth, you don't have to remember anything.",
+        author: "Mark Twain",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "We accept the love we think we deserve.",
+        author: "Stephen Chbosky, The Perks of Being a Wallflower",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "When you reach the end of your rope, tie a knot in it and hang on.",
+        author: "Franklin D. Roosevelt",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        author: "Robert Louis Stevenson",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt",
     },
     {
-        quotes: '',
-        author: '',
+        quotes: "Whoever is happy will make others happy too.",
+        author: "Anne Frank",
     },
 ];
