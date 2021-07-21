e98a5325d2bde1bd14244d1583d004247560d493
Gakyeong <qeiqiem@gmail.com>
2021-07-22 01:35:32 +0900

    confirm delete using filter()

--- js/todo.js
+++ js/todo.js
@@ -3,13 +3,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = []; // newTodo를 담을 그릇
-/* but LS는 array 형태로 저장할 수 X. only save TEXT. 문자만 저장 가능.
- 그래서 stringify를 하지 않고 LS에 저장하면 value가 a, b, c 이런 텍스트 형태로 저장이 된다.
+// newTodo를 담을 그릇
+// const toDos = [];
+// 1. const []로 array가 비어있는 상태로 선언했고, 아래에서 push()를 이 상태에 계속 해 주고 있기 때문에 새로고침하면 기존 정보가 덮어써지게 됨
 
- json.stringify() => array자체를 문자열로 저장
- json.parse() => array형태로 재변경
-*/
+// 2. => let으로 변환해서 추가할 수 있도록 만듦
+let toDos = [];
 
 const TODOS_KEY = "todos";
 
@@ -17,16 +16,27 @@ const TODOS_KEY = "todos";
 function saveToDo() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
+/* LS는 array 형태로 저장할 수 X. 
+ only save TEXT. 문자만 저장 가능.
+ 그래서 stringify를 하지 않고 LS에 저장하면 value가 a, b, c 이런 텍스트 형태로 저장이 된다.
 
-function paintToDo(newToDo) {
+ json.stringify() => array자체를 문자열로 저장
+ json.parse() => array형태로 재변경
+*/
+
+function paintToDo(newToDoObj) {
     const li = document.createElement("li");
+    li.id = newToDoObj.id; //어떤 li를 지울건지 파악하기 위해 random으로 정해지는 id값을 li에 부여해줌
+
     const span = document.createElement("span");
+    span.innerText = newToDoObj.text;
+
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
+
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newToDo;
     toDoList.appendChild(li);
 }
 
@@ -34,8 +44,14 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = ""; // enter시 todo input창 비워줌
-    toDos.push(newToDo); //toDos Array에 저장 (LS에 저장하기 위해)
-    paintToDo(newToDo); // todo list를 생성해서 보이게 함
+    // DB remove (array 저장 정보)를 위해 obj로 정보를 저장해 아이디값으로 삭제하도록 함.
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now(),
+    };
+
+    toDos.push(newToDoObj); //toDos Array에 저장 (LS에 저장하기 위해)
+    paintToDo(newToDoObj); // todo list를 생성해서 보이게 함
     saveToDo(); // todos LS에 svae
 }
 
@@ -48,17 +64,32 @@ function deleteToDo(event) {
     // innerText로 console 출력해보면 "click"시 어느 list가 parent인지 파악할 수 있다.
     // console.log(event.target.parentElement.innerText);
     const li = event.target.parentElement;
+    // ==> target.parentElement로 id를 추출할 수 있음
+    // console.log(li.id);
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    // toDo === 인자로 들어오는 toDos[]의 i번째 값들
+
+    // li.id => string (document안의 Element이기 때문에)
+    // toDo.id => number (Date함수로 생성했기 때문에)
+    saveToDo(); // 필수!!!!
 }
+
+/* filter 를 이용해서 새로운 array로 덮어쓰기를 한다. (pop을 쓸 수 없으니까.. )
+filter는 forEach 로직처럼 작동한다.
+반복적으로 해당 함수를 호출해서 배열의 index를 순차적으로 대입한다.
+filter는 true를 리턴해야 덮어쓰기처럼 쓸 수 있댜! (=해당 인덱스를 유지할 수 있다)
+*/
 
 const SavedToDos = localStorage.getItem(TODOS_KEY); // => just STRING
 
 if (SavedToDos) {
     const parseToDos = JSON.parse(SavedToDos); // => ARRAY
-    // array에서 각 개체당 실행하도록 반복하는 api. 반복문!
-    parseToDos.forEach((items) => console.log(items));
+    toDos = parseToDos;
+    parseToDos.forEach(paintToDo); // array에서 각 개체당 실행하도록 반복하는 api. 반복문!
 }
 
 // function items(item) {
-//     console.log("name", item); // 순차적으로 지금 처리되는 아이템이 무엇인지 알 수 있음. event처럼 js가 제공해주는 기능!
+//     console.log("name", item);
+// 순차적으로 지금 처리되는 아이템이 무엇인지 알 수 있음. event처럼 js가 제공해주는 기능!
 // }
